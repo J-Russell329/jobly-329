361844451f534d49974e9e9bc85dfbcc4c487aa6
root <jacob@JacobPC.localdomain>
2021-09-20 20:32:41 -0500

    added in some config files for API in production mode

--- src/config.js
+++ src/config.js
@@ -1,7 +1,13 @@
 /** Shared config for application; can be required many places. */
 
 const SECRET_KEY = 'secret-dev';
-const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+let BASE_URL;
+
+if (NODE_ENV === 'production') {
+	BASE_URL = 'https://jobly-backend-329.herokuapp.com/';
+} else {
+	BASE_URL = 'http://localhost:3001';
+}
 
 module.exports = {
 	SECRET_KEY,
