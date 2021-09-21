816da750cfe1c143d4d119211b23572c30595e53
root <jacob@JacobPC.localdomain>
2021-09-20 21:44:45 -0500

    changed the base_url... it should work now

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /** Shared config for application; can be required many places. */
 
 const SECRET_KEY = 'secret-dev';
-let BASE_URL = 'https://jobly-backend-329.herokuapp.com/';
+let BASE_URL = 'https://jobly-backend-329.herokuapp.com';
 
 module.exports = {
 	SECRET_KEY,
